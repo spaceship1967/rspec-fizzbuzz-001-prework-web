76839d2495f6bea8693a1e89c017b34853bb71ec
Michael Wiggins <michael.wiggins@me.com>
2016-08-13 17:01:05 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,3 +1,7 @@
+#again, the instructions are verbose (probably on purpose) and your way of reading focuses on the wrong things.
+#Your way of reading just confuses the issue. Adjust to the new habits of mind. Think like a programmer.
+#Don't pay attention to extraneous stuff, just try to understand the conditions and account for each of them in turn.
+#If this, then that.
 def fizzbuzz(int)
 
   if int % 3 == 0 && int % 5 == 0
